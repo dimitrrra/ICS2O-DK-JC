ede94f56fd1eff990cd8809d7709961e4b461f5b
JadenColtart <42980036+JadenColtart@users.noreply.github.com>
2018-10-22 13:58:50 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,194 @@
+
+THIS WILL BE WHERE WE TRACK OUR PROJECT AND HOST OUR EXPERIENCE
+
+#1-Emma
+
+1)t are your hobbies?
+
+Play Videos
+Gymnastics
+Hockey
+
+2)What are your favourite games?
+
+Fortnite
+Minecraft
+
+3)What is your favourite subject in school?
+
+French
+Math
+Science
+
+4)What is your favourite tv show and who is your favourite character?
+
+Goosebumps
+Victourious
+Sams Cat
+
+
+5)What colours do you like? (5)
+
+ Black
+ White 
+ Gray
+ Purple
+
+#2-Luke
+
+1)What are your hobbies?
+
+Playing Sports
+Video games
+Practice
+Soccer
+
+2)What are your favourite games?
+
+WWE 2016
+Fifa 2017
+WII
+Capture the Flag
+
+3)What is your favourite subject in school?
+
+Math 
+French
+Science
+
+4)What is your favourite tv show and who is your favourite character?
+
+Stone Age Smackdown
+
+5)What colours do you like? (5)
+ 
+ Green
+ Orange
+ Red
+ 
+6)What kind of art would you like you to put into the game
+
+Wrestling arena
+
+7)What is your favourite animal?
+
+Hamsters
+Dogs
+Fish
+
+8)Is there anything else you want to put into the game?
+
+Wrestling 
+Seth Rollins
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+
+ 
+ 
+ 
+
+
+
+
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
