ed66a7eacb00949fe7fb001ec520fe8d8181ab17
dimitrrra <31707265+dimitrrra@users.noreply.github.com>
2019-01-17 14:57:38 -0500

    Update pipe.js

--- pipe.js
+++ pipe.js
@@ -1,20 +1,22 @@
-function Pipe() {
+function Pipe(x) {
   //this.pos = createVector(50, height);
   //this.vel = createVector(1, 0);
   //this.acc = createVector(0, 0);
-  this.length = 1200;
-  this.width = 60;
-  this.x = 700;
+  this.len = 900;
+  this.size = 60;
+  this.x = x;
   this.y = 300-50;
+  this.color = random(0,255);
+  this.sep = 125;
  
   this.applyForce = function(force) {
     this.acc.add(force);
   }
 
   this.update = function() {
-    this.x--;
-    if (this.x<-0) {
-      this.x= 1500;
+    this.x=this.x-4;
+    if (this.x<-80) {
+      this.x= 800;
     }
      // this.vel.add(this.acc);
      // this.pos.add(this.vel);
@@ -22,17 +24,17 @@ function Pipe() {
   }
 
   this.display = function() {
-    fill(0, 38, 255);
+    fill(color, 38, 255);
     stroke(255);
+   
+    rect(this.x, this.y, this.size, this.len);
+    rect(this.x, this.y-this.sep, this.size, this.len-1000);
     
-    rect(this.x, this.y, this.width, this.length);
-    rect(this.x, this.y-350, this.width, this.length-1000);
+    //rect(this.x+350,this.y+75, this.width,this.length);
+    //rect(this.x+350, this.y-275,this.width,this.length-1000);
     
-    rect(this.x+350,this.y+75, this.width,this.length);
-    rect(this.x+350, this.y-275,this.width,this.length-1000);
-    
-    rect(this.x+700, this.y-15,this.width,this.length);
-    rect(this.x+700,this.y-155,this.width,this.length-1500);
+    //rect(this.x+700, this.y-15,this.width,this.length);
+    //rect(this.x+700,this.y-155,this.width,this.length-1500);
   }
 
   this.edges = function() {
