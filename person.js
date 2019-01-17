4a74d415f2f3d35acacafde7d7928f00c79faf7a
dimitrrra <31707265+dimitrrra@users.noreply.github.com>
2019-01-17 14:57:11 -0500

    Update person.js

--- person.js
+++ person.js
@@ -1,4 +1,7 @@
 function Person() {
+  this.x=height/2;
+  this.colour = color(100, 25, 67);
+
   this.pos = createVector(25, height);
   this.vel = createVector(1, 0);
   this.acc = createVector(0, 0);
@@ -14,9 +17,11 @@ function Person() {
   }
 
   this.display = function() {
-    fill(10, 255, 67);
+    fill(this.colour);
     stroke(255);
-    ellipse(this.pos.x, this.pos.y-15, 30, 30);
+     ellipse(this.x, this.pos.y-15, 30, 30);
+
+    //ellipse(this.pos.x, this.pos.y-15, 30, 30);
   }
 
   this.edges = function(pipe) {
@@ -33,4 +38,35 @@ function Person() {
     if((pipe.y-50<person.y || pipe.y+50>person.y)&&(pipe.x-50<person.x || pipe.x+50>person.x)){
 }
   }
+
+this.checkPipe = function(p){
+//var rect1 = {x: 5, y: 5, width: 50, height: 50} "this" person
+//var rect2 = {x: 20, y: 10, width: 10, height: 10} pipe 
+   //fill(255,255,0);
+     //rect(30,30,10,10);
+     console.log(p.y);
+     console.log(this.y);
+if (this.x > p.x && this.x < p.size + p.x &&(this.pos.y > p.y ||
+   this.pos.y < p.y - p.sep)) {
+     this.colour = color(255,0,0);
+   //background(255,0,0);
+     rect(30,30,10,10 ); //shows up whenever the man hits the bar
+}else{rect(10,10,10,10);
+
+
+
+}
+if(this.y>p.y){
+       rect(50,50,10,10);
+
+}
+// filling in the values =>
+
+//if (5 < 30 &&
+//    55 > 20 &&
+//    5 < 20 &&
+//    55 > 10) {
+//    // collision detected!
+//}
+}
 }
